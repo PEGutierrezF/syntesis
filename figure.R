9fdda73259c385cbf6603edf4fa6a468ee661c6d
Pablo E. Gutierrez-Fonseca <pabloe.gutierrezfonseca@gmail.com>
2023-07-01 16:24:53 -0400

    data

--- figure.R
+++ figure.R
@@ -1,6 +1,22 @@
 
 
 
+
+# ---------------------------------------------
+# Figure 1
+# 01 Jul 2023
+# Pablo E. Gutiérrez-Fonseca
+# pabloe.gutierrezfonseca@gmail.com
+# ---------------------------------------------
+#  
+
+
+
+
+
+
+
+
 library(ggplot2)
 library(tidyr)
 library(readxl)
